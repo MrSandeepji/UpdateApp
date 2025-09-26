b9cbee3eb8e3205769f358ffda29f572f8cda6bd
MrSandeepji <142229130+MrSandeepji@users.noreply.github.com>
2025-09-26 19:23:34 +0530

    Update AppInfoDialog.java

--- AppInfoDialog.java
+++ AppInfoDialog.java
@@ -1,16 +1,9 @@
 [
-  {
-    "pkg": "com.sk.forceupdate",
-    "version": "1.10",
-    "update_url": "https://google.com",
-    "upBtn": "Update",
-    "msg": "New version available! Update for new features."
-  },
-  {
-    "pkg": "com.cinehd",
-    "version": "1.0",
-    "update_url": "https://google.com",
-    "upBtn": "Update",
-    "msg": "New version available! Update for new features."
-  }
+{
+  "packageName": "com.sk.dataimport",
+  "version": "1.0",
+  "title": "Please Attention",
+  "message": "Please clear app data if you are facing any issue\n\nIf you have already cleared then click on done button",
+  "dialogEnabled": true
+}
 ]
