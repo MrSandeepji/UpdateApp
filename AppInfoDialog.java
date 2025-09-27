d4ea2e5993f35d6f58c9f5d4f8810a705ad90a57
MrSandeepji <142229130+MrSandeepji@users.noreply.github.com>
2025-09-27 08:10:38 +0530

    Update AppInfoDialog.java

--- AppInfoDialog.java
+++ AppInfoDialog.java
@@ -5,5 +5,12 @@
   "title": "Please Attention",
   "message": "Please clear app data if you are facing any issue\n\nIf you have already cleared then click on done button",
   "dialogEnabled": true
+},
+{
+  "packageName": "in.startv.hotstar",
+  "version": "25.07.07.0",
+  "title": "Please Attention",
+  "message": "Please clear app data if you are facing any issue\n\nIf you have already cleared then click on done button",
+  "dialogEnabled": true
 }
 ]
